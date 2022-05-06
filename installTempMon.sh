7c47d921a2a8f2c0502aab2d9b60bef116527796
nikbold88 <90900186+nikbold88@users.noreply.github.com>
2022-05-06 09:49:47 +0100

    create dht22.py
    Created dht22.py and set it to executable

--- installTempMon.sh
+++ installTempMon.sh
@@ -11,7 +11,29 @@ sudo python3 -m pip install --upgrade pip setuptools wheel
 # Install the Adafruit_DHT library
 sudo pip3 install Adafruit_DHT
 # Create DHT22 script
+tee -a /tmp/dht22.py <<EOF
+import Adafruit_DHT
+from time import sleep
+sensor = Adafruit_DHT.DHT22
+# DHT22 sensor connected to GPIO12.
+pin = 12
+print("[press ctrl+c to end the script]")
+try: # Main program loop
+   while True:
+     humidity, temperature = Adafruit_DHT.read_retry(sensor,
+pin)
+     sleep(2.5)
+     if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%"
+                           .format(temperature, humidity))
+     else:
+        print("Failed to get reading. Try again!")
+# Scavenging work after the end of the program
+except KeyboardInterrupt: print("Script end!")
+chmod +x /tmp/dht22.py
+EOF
 # Test the DHT22 script
+python3 /tmp/dht22.py
 ### Setting up InfluxDb and telegraf
 # InfluxDb Installation on Raspberry Pi
 # Telgraf log parser Installation on Raspberry Pi
